6e9064dadf0b185d7d93d9acfd591ea94d0615b4
D Bundschuh <78927864+DiBundschuh@users.noreply.github.com>
2022-10-01 01:19:54 +0200

    Stage 1
    Beginning. I made a dictionary out of the statements, sorted it by hand and looped over the dictionary

--- LoanCalculator.py
+++ LoanCalculator.py
@@ -1,3 +1,10 @@
-"""
-TEST
-"""
+statements = {
+    "loan_principal": 'Loan principal: 1000',
+    "first_month":'Month 1: repaid 250',
+    "second_month": 'Month 2: repaid 250',
+    "third_month": 'Month 3: repaid 500',
+    "final_output": 'The loan has been repaid!'}
+# write your code here
+
+for statement in statements:
+    print(statements[statement])
